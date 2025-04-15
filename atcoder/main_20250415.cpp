f638ba96885e2b2e09e37a1491ce8b7467aa9100
Zuiverloon <junyao43.zhang@outlook.com>
2025-04-15 21:36:39 +0800

    2025041

---
+++ atcoder/main_20250415.cpp
@@ -0,0 +1,205 @@
+/**
+ * https://atcoder.jp/contests/abc347/tasks/abc347_d
+ */
+
+#include <functional> //c++17 function
+#include <vector>
+#include <string.h>
+#include <string>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <limits.h>
+#include <cctype>
+#include <chrono>
+#include <utility>
+#include <list>
+#include <stdio.h>
+#include <iostream>
+
+// #include <bits/stdc++.h>
+
+typedef long long ll;
+using namespace std;
+
+// clang++ -std=c++17 main.cpp -o main
+
+ll mod1e9 = 1000000007LL;
+ll mod998 = 998244353LL;
+
+ll gcd(ll a, ll b)
+{
+    if (a < b)
+    {
+        return gcd(b, a);
+    }
+    if (b == 0)
+    {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+void printVector(std::vector<ll> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        std::cout << v[i] << " \n"[i == v.size() - 1];
+        // printf("%lld%c", v[i], " \n"[i == v.size() - 1]);
+    }
+}
+
+void printVector(std::vector<std::string> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        std::cout << v[i] << " \n"[i == v.size() - 1];
+        // printf("%s%c", v[i].c_str(), " \n"[i == v.size() - 1]);
+    }
+}
+
+template <typename T>
+void printMatrix(std::vector<std::vector<T>> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        printVector(v[i]);
+    }
+}
+
+template <typename T>
+bool isinset(std::set<T> &s, T n) // 判断是否在set内
+{
+    return s.find(n) != s.end();
+}
+
+template <typename T>
+bool isinset(std::unordered_set<T> &s, T n) // 判断是否在set内
+{
+    return s.find(n) != s.end();
+}
+
+void ppqq()
+{
+    // 	auto cmp = [](int a, int b) -> bool
+    // 	{ return a < b; };
+    // 	std::priority_queue<int, std::vector<int>, std::function<bool(int a, int b)>> q(cmp);
+}
+
+ll lowbit(ll a)
+{
+    return a & (-a);
+}
+
+ll highbit(ll a)
+{
+    ll lb = lowbit(a);
+    while (lb != a)
+    {
+        a -= lb;
+        lb = lowbit(a);
+    }
+    return lb;
+}
+
+ll countone(ll a)
+{
+    ll ans = 0;
+    while (a > 0)
+    {
+        ans += (a & 1);
+        a >>= 1;
+    }
+    return ans;
+}
+
+void exgcd(ll a, ll b, ll &x, ll &y)
+{
+    if (b == 0)
+    {
+        x = 1, y = 0;
+        return;
+    }
+    exgcd(b, a % b, y, x);
+    y -= a / b * x;
+}
+
+#define FOR(i, s, j, o) for (ll i = s; i < j; i += o)
+#define FORR(i, s, j, o) for (ll i = s; i > j; i += o)
+
+void solve(int cas)
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+    ll cc = countone(c);
+    ll ansa = 0, ansb = 0;
+    ll tc = c;
+    ll base = 1LL;
+    if (cc > a + b)
+    {
+        cout << "-1\n";
+        return;
+    }
+    while (tc > 0)
+    {
+        if ((tc & 1LL) == 1LL)
+        {
+            if (a > b)
+            {
+                ansa += base;
+                a--;
+            }
+            else
+            {
+                ansb += base;
+                b--;
+            }
+        }
+        tc >>= 1LL;
+        base <<= 1LL;
+    }
+
+    if (tc != 0 || a != b || a < 0 || b < 0)
+    {
+        cout << "-1\n";
+        return;
+    }
+    base = 1LL;
+    ll limit = (1LL) << 60;
+    while (a > 0 && base < limit)
+    {
+        if ((base & c) == 0)
+        {
+            a--;
+            ansa += base;
+            ansb += base;
+        }
+        base <<= 1LL;
+    }
+    if (a > 0)
+        cout << "-1\n";
+    else
+        cout << ansa << " " << ansb << "\n";
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    // initmobelong();
+
+    int n = 1;
+    // std::cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        solve(i);
+    }
+    return 0;
+}
