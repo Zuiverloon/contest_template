b575de33a947ec220acb98dc25a11c06d7a95c22
Zuiverloon <junyao43.zhang@outlook.com>
2025-05-03 23:10:06 +0800

    20250503

---
+++ atcoder/abc404/b.cpp
@@ -0,0 +1,224 @@
+
+
+#include <functional> //c++17 function
+#include <vector>
+#include <string.h>
+#include <string>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <limits.h>
+#include <cctype>
+#include <chrono>
+#include <utility>
+#include <list>
+#include <stdio.h>
+#include <iostream>
+
+// #include <bits/stdc++.h>
+
+typedef long long ll;
+using namespace std;
+
+// clang++ -std=c++17 main.cpp -o main
+
+ll mod1e9 = 1000000007LL;
+ll mod998 = 998244353LL;
+ll mod1e8 = 100000000LL - 3;
+
+ll gcd(ll a, ll b)
+{
+    if (a < b)
+    {
+        return gcd(b, a);
+    }
+    if (b == 0)
+    {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+void printVector(std::vector<ll> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        std::cout << v[i] << " \n"[i == v.size() - 1];
+        // printf("%lld%c", v[i], " \n"[i == v.size() - 1]);
+    }
+}
+
+void printVector(std::vector<std::string> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        std::cout << v[i] << " \n"[i == v.size() - 1];
+        // printf("%s%c", v[i].c_str(), " \n"[i == v.size() - 1]);
+    }
+}
+
+template <typename T>
+void printMatrix(std::vector<std::vector<T>> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        printVector(v[i]);
+    }
+}
+
+template <typename T>
+bool isinset(std::set<T> &s, T n) // 判断是否在set内
+{
+    return s.find(n) != s.end();
+}
+
+template <typename T>
+bool isinset(std::unordered_set<T> &s, T n) // 判断是否在set内
+{
+    return s.find(n) != s.end();
+}
+
+void ppqq()
+{
+    // 	auto cmp = [](int a, int b) -> bool
+    // 	{ return a < b; };
+    // 	std::priority_queue<int, std::vector<int>, std::function<bool(int a, int b)>> q(cmp);
+}
+
+ll lowbit(ll a)
+{
+    return a & (-a);
+}
+
+ll highbit(ll a)
+{
+    ll lb = lowbit(a);
+    while (lb != a)
+    {
+        a -= lb;
+        lb = lowbit(a);
+    }
+    return lb;
+}
+
+ll countone(ll a)
+{
+    ll ans = 0;
+    while (a > 0)
+    {
+        ans += (a & 1);
+        a >>= 1;
+    }
+    return ans;
+}
+
+void exgcd(ll a, ll b, ll &x, ll &y)
+{
+    if (b == 0)
+    {
+        x = 1, y = 0;
+        return;
+    }
+    exgcd(b, a % b, y, x);
+    y -= a / b * x;
+}
+
+/***     dsu template           */
+vector<ll> fa;
+
+ll get(ll i)
+{
+    if (fa[i] != i)
+    {
+        fa[i] = get(fa[i]);
+    }
+    return fa[i];
+}
+
+void merge(ll a, ll b)
+{
+    fa[get(b)] = get(a);
+}
+
+/***     dsu template           */
+
+#define FOR(i, s, j, o) for (ll i = s; i < j; i += o)
+#define FORR(i, s, j, o) for (ll i = s; i > j; i += o)
+
+void rotate(vector<string> &s, vector<string> &st)
+{
+    for (ll i = 0; i < s.size(); i++)
+    {
+        for (ll j = 0; j < s.size(); j++)
+        {
+            st[i][j] = s[s.size() - 1 - j][i];
+        }
+    }
+}
+
+ll diff(vector<string> &s, vector<string> &st)
+{
+    ll ans = 0;
+    for (ll i = 0; i < s.size(); i++)
+    {
+        for (ll j = 0; j < s.size(); j++)
+        {
+            if (s[i][j] != st[i][j])
+            {
+                ans++;
+            }
+        }
+    }
+    return ans;
+}
+
+void solve(int cas)
+{
+    ll n;
+    cin >> n;
+
+    vector<string> s(n, "");
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> s[i];
+    }
+    vector<string> t(n, "");
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> t[i];
+    }
+    ll ans = INT_MAX;
+    ans = min({ans, diff(s, t)});
+    vector<string> vs1(s);
+    rotate(s, vs1);
+    ans = min({ans, diff(vs1, t) + 1});
+    vector<string> vs2(s);
+    rotate(vs1, vs2);
+    ans = min({ans, diff(vs2, t) + 2});
+    vector<string> vs3(s);
+    rotate(vs2, vs3);
+    ans = min({ans, diff(vs3, t) + 3});
+    cout << ans << "\n";
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    // initmobelong();
+
+    int n = 1;
+    // std::cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        solve(i);
+    }
+    return 0;
+}
